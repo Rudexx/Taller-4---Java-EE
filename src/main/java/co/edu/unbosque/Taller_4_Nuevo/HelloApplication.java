73e3bde6f07bfc1af1d8c9c26cb5813ac8ba111f
Rudexx <65428804+Rudexx@users.noreply.github.com>
2021-05-03 22:56:07 -0500

    arreglado

---
+++ src/main/java/co/edu/unbosque/Taller_4_Nuevo/HelloApplication.java
@@ -0,0 +1,9 @@
+package co.edu.unbosque.Taller_4_Nuevo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/com/example/Taller_4/PruebaCookie.java
@@ -0,0 +1,39 @@
+package co.edu.unbosque.Taller_4_Nuevo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "prueba", value = "/prueba")
+public class PruebaCookie extends HttpServlet {
+
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+        resp.setContentType("text/html");
+        String userName = req.getParameter("usuario");
+
+        try{
+            PrintWriter out = resp.getWriter();
+
+            Cookie c1 = new Cookie("usuario" , userName);
+            resp.addCookie(c1);
+
+            out.println("<a href='main.html'> Ingreso de Imagen</a> ");
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void destroy() {
+    }
+}
