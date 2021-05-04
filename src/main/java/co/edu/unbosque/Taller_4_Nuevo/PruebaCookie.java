fa44d90680d440fb0f42e73f80de730067d301ed
Rudexx <65428804+Rudexx@users.noreply.github.com>
2021-05-03 23:15:55 -0500

    no se que pasa

--- src/main/java/co/edu/unbosque/Taller_4_Nuevo/PruebaCookie.java
+++ src/main/java/co/edu/unbosque/Taller_4_Nuevo/PruebaCookie.java
@@ -1,12 +1,10 @@
 package co.edu.unbosque.Taller_4_Nuevo;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "prueba", value = "/prueba")
